64747c438d82b88ff8a2e524ddeb100a2f000c47
ewertonvaz <ewerton.vaz@gmail.com>
2013-10-12 14:50:02 -0300

    Commit lamp v.1.02

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-./linux/linux.sh
-./apache/apache.sh
-./mysql/mysql.sh
-./php/php.sh
+sudo apt-get update
+
+##LINUX
+sudo apt-get install -y unzip
+sudo apt-get install -y vsftpd
+
+##APACHE
+sudo apt-get install -y apache2 libssl1.0.0
+
+##MYSQL
+sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
+
+##PHP
+sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install -y php5-curl php5-gd php5-odbc
+
+
 
 
 
