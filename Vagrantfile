cf725ea8cedd23b340bc291a9406ecb3260f1b5c
ewertonvaz <ewerton.vaz@gmail.com>
2013-10-12 23:13:19 -0300

    Outro commit

--- Vagrantfile
+++ Vagrantfile
@@ -8,5 +8,4 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu32"
   config.vm.provision :shell, :path => "bootstrap.sh"
   config.vm.network :forwarded_port, host: 8080, guest: 80
-  
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,11 @@ sudo apt-get install -y vsftpd
 sudo apt-get install -y apache2 libssl1.0.0
 
 ##MYSQL
+sudo apt-get update
 sudo apt-get install -y mysql-server libapache2-mod-auth-mysql php5-mysql
 
 ##PHP
+sudo apt-get update
 sudo apt-get install -y php5 libapache2-mod-php5 php5-mcrypt
 sudo apt-get install -y php5-curl php5-gd php5-odbc
 
